355c56f30682aef68f423eb2266ce26df1ab80e8
陈海明 <jxtc163@163.com>
2022-11-15 15:01:06 +0800

    插件加载工程

--- src/test/java/org/thingsboard/server/PluginDemoApplicationTests.java
+++ src/test/java/org/thingsboard/server/PluginDemoApplicationTests.java
@@ -1,13 +1,12 @@
 package org.thingsboard.server;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PluginDemoApplicationTests {
-
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(PluginDemoApplicationTests.class, args);
     }
 
 }

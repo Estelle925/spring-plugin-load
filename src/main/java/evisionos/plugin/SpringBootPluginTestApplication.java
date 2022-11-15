cbb7de25540914a7d52efcc11f8666b286efbd0e
陈海明 <jxtc163@163.com>
2022-11-15 19:47:59 +0800

    配置条件

---
+++ src/main/java/evisionos/plugin/SpringBootPluginTestApplication.java
@@ -0,0 +1,11 @@
+package evisionos.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPluginTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPluginTestApplication.class, args);
+    }
+}

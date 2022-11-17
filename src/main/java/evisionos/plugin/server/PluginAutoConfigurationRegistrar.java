b74749beb1960536fde605cecfbbf36e57816842
陈海明 <jxtc163@163.com>
2022-11-17 16:00:59 +0800

    多插件 混插入模式

--- src/main/java/evisionos/plugin/server/PluginAutoConfigurationRegistrar.java
+++ src/main/java/evisionos/plugin/server/PluginAutoConfigurationRegistrar.java
@@ -28,46 +28,50 @@ import java.util.stream.Stream;
 public class PluginAutoConfigurationRegistrar implements ImportBeanDefinitionRegistrar, EnvironmentAware {
 
     String moduleJarAbsolutePath;
+
     @Override
-    public void registerBeanDefinitions(@NonNull AnnotationMetadata annotationMetadata,@NonNull BeanDefinitionRegistry beanDefinitionRegistry) {
-        //获取目录下jar包
-        List<String> jarPaths = Lists.newArrayList();
-        try {
-            Stream<Path> paths = Files.walk(Paths.get(moduleJarAbsolutePath), 2);
-            paths.map(Path::toString).filter(f -> f.endsWith(".jar")).forEach(jarPaths::add);
-            paths.close();
-        } catch (Exception e) {
-            log.error("load jars packages from path fail exception = ", e);
-        }
+    public void registerBeanDefinitions(@NonNull AnnotationMetadata annotationMetadata, @NonNull BeanDefinitionRegistry beanDefinitionRegistry) {
+        if (moduleJarAbsolutePath != null && !moduleJarAbsolutePath.isEmpty() && !moduleJarAbsolutePath.trim().isEmpty()) {
 
-        //加载jar包
-        if (CollectionUtil.isNotEmpty(jarPaths)) {
-            for (String path : jarPaths) {
-                Path jarPath = Paths.get(path);
-                try {
-                    URL[] urls = new URL[]{jarPath.toUri().toURL()};
-                    URLClassLoader urlClassLoader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
-                    List<PluginConfig> pluginConfigList = new ArrayList<>();
-                    ServiceLoader<PluginConfig> loadedDrivers = ServiceLoader.load(PluginConfig.class, urlClassLoader);
-                    loadedDrivers.forEach(pluginConfigList::add);
-                    if (pluginConfigList.size() != 1) {
-                        log.error("plugin config has and only has one");
-                        continue;
-                    }
+            //获取目录下jar包
+            List<String> jarPaths = Lists.newArrayList();
+            try {
+                Stream<Path> paths = Files.walk(Paths.get(moduleJarAbsolutePath), 2);
+                paths.map(Path::toString).filter(f -> f.endsWith(".jar")).forEach(jarPaths::add);
+                paths.close();
+            } catch (Exception e) {
+                log.error("load jars packages from path fail exception = ", e);
+            }
 
-                    PluginConfig pluginConfig = pluginConfigList.get(0);
-                    Set<Class<?>> classes = ClassUtil.getClasses(urlClassLoader, path);
-                    for (Class<?> aClass : classes) {
-                        if (SpringUtils.isSpringBeanClass(aClass)) {
-                            SpringUtils.registerBean(aClass,beanDefinitionRegistry);
+            //加载jar包
+            if (CollectionUtil.isNotEmpty(jarPaths)) {
+                for (String path : jarPaths) {
+                    Path jarPath = Paths.get(path);
+                    try {
+                        URL[] urls = new URL[]{jarPath.toUri().toURL()};
+                        URLClassLoader urlClassLoader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
+                        List<PluginConfig> pluginConfigList = new ArrayList<>();
+                        ServiceLoader<PluginConfig> loadedDrivers = ServiceLoader.load(PluginConfig.class, urlClassLoader);
+                        loadedDrivers.forEach(pluginConfigList::add);
+                        if (pluginConfigList.size() != 1) {
+                            log.error("plugin config has and only has one");
+                            continue;
                         }
-                        if (SpringUtils.isSpringController(aClass)){
-                            SpringUtils.registerController(aClass,beanDefinitionRegistry);
+
+                        PluginConfig pluginConfig = pluginConfigList.get(0);
+                        Set<Class<?>> classes = ClassUtil.getClasses(urlClassLoader, path);
+                        for (Class<?> aClass : classes) {
+                            if (SpringUtils.isSpringBeanClass(aClass)) {
+                                SpringUtils.registerBean(aClass, beanDefinitionRegistry);
+                            }
+                            if (SpringUtils.isSpringController(aClass)) {
+                                SpringUtils.registerController(aClass, beanDefinitionRegistry);
+                            }
                         }
+                        log.info("Load plugin success: name={}, version={}, jarPath={}", pluginConfig.name(), pluginConfig.version(), jarPath);
+                    } catch (Exception e) {
+                        log.error(String.format("Load plugin exception, jarPath=%s", jarPath), e);
                     }
-                    log.info("Load plugin success: name={}, version={}, jarPath={}", pluginConfig.name(), pluginConfig.version(), jarPath);
-                } catch (Exception e) {
-                    log.error(String.format("Load plugin exception, jarPath=%s", jarPath), e);
                 }
             }
         }

74cfbf57d51ad46fbf11b6fd8e75d2dc2917a8ac
陈海明 <jxtc163@163.com>
2023-03-31 15:35:27 +0800

    插件实体类调整

--- src/main/java/com/plugin/server/Plugin.java
+++ src/main/java/com/plugin/server/Plugin.java
@@ -26,7 +26,6 @@ import static com.google.common.base.Preconditions.checkState;
 @AllArgsConstructor
 public class Plugin {
 
-    private final Map<String, PluginHandler> handlers;
     private Path jarPath;
     private PluginConfig pluginConfig;
     private PluginApplicationContext pluginApplicationContext;
@@ -36,55 +35,7 @@ public class Plugin {
         this.jarPath = jarPath;
         this.pluginConfig = pluginConfig;
         this.pluginApplicationContext = pluginApplicationContext;
-        this.handlers = scanHandlers();
         this.mvcController = mvcController;
-    }
-
-    private Map<String, PluginHandler> scanHandlers() {
-        Map<String, PluginHandler> handlers = Maps.newHashMap();
-        // find Handler in plugin
-        for (PluginHandler handler : pluginApplicationContext.getBeansOfType(PluginHandler.class).values()) {
-            String handlerName = handler.name();
-            if (!StringUtils.hasText(handlerName)) {
-                throw new PluginRuntimeException("scanHandlers handlerName is null");
-            }
-            checkState(!handlers.containsKey(handlerName), "Duplicated handler %s found by: %s",
-                    PluginHandler.class.getSimpleName(), handlerName);
-            handlers.put(handlerName, handler);
-        }
-        if (log.isInfoEnabled()) {
-            log.info("Scan handlers finish: {}", String.join(",", handlers.keySet()));
-        }
-        return ImmutableMap.copyOf(handlers);
-    }
-
-    public PluginHandler getHandler(String handlerName) {
-        checkNotNull(handlerName, "handlerName is null");
-        PluginHandler handler = handlers.get(handlerName);
-        checkNotNull(handler, String.format("find handler is null, handlerName=%s", handlerName));
-        return handler;
-    }
-
-    public Object doHandler(String handlerName, String handlerArgs) {
-        checkNotNull(handlerName, "handlerName is null");
-        checkNotNull(handlerArgs, "handlerArgs is null");
-        return doHandlerWithinPluginClassLoader(getHandler(handlerName), handlerArgs);
-    }
-
-    private Object doHandlerWithinPluginClassLoader(PluginHandler handler, String handlerArgs) {
-        checkNotNull(handler, "handler is null");
-        checkNotNull(handlerArgs, "handlerArgs is null");
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        try {
-            ClassLoader pluginClassLoader = handler.getClass().getClassLoader();
-            Thread.currentThread().setContextClassLoader(pluginClassLoader);
-            return handler.execute(handlerArgs);
-        } catch (Exception e) {
-            log.error(String.format("Invoke plugin exception, handler=%s", handler.name()), e);
-            throw new PluginRuntimeException(String.format("doHandlerWithinPluginClassLoader has error, handler=%s", handler.getClass().getName()), e);
-        } finally {
-            Thread.currentThread().setContextClassLoader(classLoader);
-        }
     }
 
     public void destroy() throws Exception {
